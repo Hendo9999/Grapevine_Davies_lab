a342aca9d0129f916856c55f0898515ab38c0593
Henderson <hen232@csiro.au>
2019-11-01 12:05:49 +1030

    format dataset- names

--- Scripts/Grapevine_Davies_lab.R
+++ Scripts/Grapevine_Davies_lab.R
@@ -6,5 +6,25 @@ library(readxl)#for reading Excel files
 
 
 #assign primary data files to variables
-Brix_Nuriootpa <- read_xlsx("Data/Nuri_Mothers_CordinateCrossReference.xlsx")
+Brix_Nuriootpa <- read_xlsx("Data/Nuri_Mothers_CordinateCrossReference.xlsx", skip = 1)
 Brix_Irymple <- read_xlsx("Data/1st_gen_varieties_development_dates_and_ripening_Irymple_site.xlsx")
+head(Brix_Nuriootpa)
+
+view(Brix_Nuriootpa)
+Brix_Nuriootpa %>% 
+ 
+
+
+
+Brix_Nuriootpa[5] <- as.character(as.numeric(Brix_Nuriootpa[5]))
+
+head(Brix_Nuriootpa)
+
+
+
+
+test <- Brix_Nuriootpa %>% 
+  ggplot(aes(x = "Current Panel Position", y = c(5:15)))+
+  geom_line()
+
+view(test)
